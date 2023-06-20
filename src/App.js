bb13ab3e388c0491568e069c6c89f26eda099a38
William Leung <gwilliamleung@gmail.com>
2023-06-20 00:07:09 -0400

    import

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
 
+import React, { useState, useEffect, useRef } from 'react'
+
 function App() {
   return (
       <div className="flex justify-center items-center h-screen bg-gray-200">
-        <div className="flex w-2/3 h-2/3 bg-white rounded-lg shadow-lg">
-          <div className="flex flex-col h-full w-1/3 rounded-l-lg  bg-gray-200">
-              <h1>hi</h1>
+        <div className="flex w-2/5 h-2/3 bg-white rounded-lg shadow-lg">
+          <div className="flex flex-col h-full w-2/5 rounded-l-lg  bg-gray-200">
+              <div className="flex w-full bg-slate-400 justify-center items-center rounded-tl-lg h-1/6">
+                Names of Bots available
+              </div>
+              <div>test</div>
+
           </div>
-          <div className="flex flex-col h-full w-auto rounded-lg  bg-gray-100">
-          </div>
+          <div className="flex flex-col h-full w-full rounded-lg bg-gray-100">
+            <div className="flex w-full bg-slate-500 rounded-tr-lg justify-center items-center h-1/6">
+                  Names of Bots available
+                </div>
+                <div>test</div>
+                <div>test</div>
+                <div>test</div>
+                <div>test</div>
+                <div>test</div>
+                <form className="mt-auto" >
+                  <div className="flex justify-between border-slate-700 border-2">
+                    <input
+                      type="text"
+                      // value={userInput}
+                      // onChange={handleInputChange}
+                      className="w-[90%] mt-auto"
+                    />
+                <button type="submit">O</button>
+              </div>
+              </form>
+
+
+            </div>
         </div>
     </div>
   );
